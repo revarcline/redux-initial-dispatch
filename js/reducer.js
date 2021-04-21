a2d00918505977fa08570a589204d8e103c49f4c
Alex Cline <rev.a.r.cline@gmail.com>
2021-04-21 01:13:01 -0500

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,19 +1,19 @@
-let state = {count: 0};
+let state;
 
-function changeState(state, action){
-    switch (action.type) {
-      case 'INCREASE_COUNT':
-        return {count: state.count + 1}
-      default:
-        return state;
-    }
+const changeState = (state = { count: 0 }, action) => {
+  switch (action.type) {
+    case "INCREASE_COUNT":
+      return { count: state.count + 1 };
+    default:
+      return state;
   }
+};
 
-function dispatch(action){
-    state = changeState(state, action)
-    render()
-}
+const dispatch = (action) => {
+  state = changeState(state, action);
+  render();
+};
 
-function render(){
-    document.body.textContent = state.count
-}
+const render = () => {
+  document.body.textContent = state.count;
+};
